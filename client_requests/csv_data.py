948898cfd7c25c6a0b2583a205a6124182f4b630
Danny Lee <datwony@gmail.com>
2022-05-29 20:32:23 -0700

    Work in progress: client side script for throughput logging

--- client_requests/csv_data.py
+++
@@ -1,8 +0,0 @@
-"""
-#datatype measurement,dateTime:RFC3339,tag,double
-m,time,type,latency
-raft,"2022-05-28T15:00:00+07:00","appendEntries",20
-raft,"2022-05-28T15:10:00+07:00","appendEntries",30
-raft,"2022-05-28T15:20:00+07:00","appendEntries",10
-raft,"2022-05-28T15:30:00+07:00","membership",15
-"""
--- client_requests/main.py
+++ client_requests/main.py
@@ -1,21 +1,22 @@
-from numpy import append
 import requests
 import constants
 import datetime
 import asyncio
 import time
-import random
 import csv
 
 DEBUG = False
 active_node_ids = []
 
-f = open('without_registry.csv', 'w')
+f = open("without_registry.csv", "w")
 writer = csv.writer(f)
+
 
 def removeSingleMember(node_id):
     private_node_ip = constants.PRIVATE_NODE_IPS[node_id]
-    response = requests.get(f"http://{constants.PUBLIC_REGISTRY_IP}:{constants.REGISTRY_PORT}/removeSingleMember?nodeId={node_id}&url=http://{private_node_ip}:{constants.NODE_PORT}")
+    response = requests.get(
+        f"http://{constants.PUBLIC_REGISTRY_IP}:{constants.REGISTRY_PORT}/removeSingleMember?nodeId={node_id}&url=http://{private_node_ip}:{constants.NODE_PORT}"
+    )
     elapsed_seconds = response.elapsed.total_seconds()
 
     active_node_ids.remove(node_id)
@@ -23,14 +24,16 @@ def removeSingleMember(node_id):
     time = datetime.datetime.now(datetime.timezone.utc).astimezone()
     if DEBUG:
         print(time.isoformat(), "remove membership", elapsed_seconds * 1000)
-    
-    return 
+
+    return
+
 
 def selectActiveNodeId():
     # Return any active node
     if len(active_node_ids) == 0:
         return -1
     return active_node_ids[0]
+
 
 def appendEntries(node_id):
     if node_id not in constants.PUBLIC_NODE_IPS.keys():
@@ -39,7 +42,9 @@ def appendEntries(node_id):
 
     public_node_ip = constants.PUBLIC_NODE_IPS[node_id]
     command = "test"
-    response = requests.get(f"http://{public_node_ip}:{constants.NODE_PORT}/add?command={command}")
+    response = requests.get(
+        f"http://{public_node_ip}:{constants.NODE_PORT}/add?command={command}"
+    )
 
     elapsed_seconds = response.elapsed.total_seconds()
     time = datetime.datetime.now(datetime.timezone.utc).astimezone()
@@ -48,13 +53,16 @@ def appendEntries(node_id):
     if DEBUG:
         print(f"appendEntries: {node_id}, {response.content}")
         print(time.isoformat(), "appendEntries", elapsed_seconds * 1000)
-    
+
     return
+
 
 def addSingleMember(node_id):
     private_node_ip = constants.PRIVATE_NODE_IPS[node_id]
-    response = requests.get(f"http://{constants.PUBLIC_REGISTRY_IP}:{constants.REGISTRY_PORT}/addSingleMember?nodeId={node_id}&url=http://{private_node_ip}:{constants.NODE_PORT}")
-    
+    response = requests.get(
+        f"http://{constants.PUBLIC_REGISTRY_IP}:{constants.REGISTRY_PORT}/addSingleMember?nodeId={node_id}&url=http://{private_node_ip}:{constants.NODE_PORT}"
+    )
+
     elapsed_seconds = response.elapsed.total_seconds()
     time = datetime.datetime.now(datetime.timezone.utc).astimezone()
 
@@ -72,15 +80,20 @@ def addSingleMember(node_id):
 def updateGroup(node_ids):
     body = {}
     for node_id in node_ids:
-        body[node_id] = f"http://{constants.PRIVATE_NODE_IPS[node_id]}:{constants.NODE_PORT}"
-    
-    response = requests.post(f"http://{constants.PUBLIC_REGISTRY_IP}:{constants.REGISTRY_PORT}/updateGroup", data=body)
+        body[
+            node_id
+        ] = f"http://{constants.PRIVATE_NODE_IPS[node_id]}:{constants.NODE_PORT}"
+
+    response = requests.post(
+        f"http://{constants.PUBLIC_REGISTRY_IP}:{constants.REGISTRY_PORT}/updateGroup",
+        data=body,
+    )
     elapsed_seconds = response.elapsed.total_seconds()
     time = datetime.datetime.now(datetime.timezone.utc).astimezone()
 
     if DEBUG:
         print(time.isoformat(), "membership", elapsed_seconds * 1000)
-    
+
     return
 
 
@@ -96,6 +109,7 @@ async def appendEntriesLoop():
             continue
         # await asyncio.sleep(0.025)
 
+
 async def membershipChangeUpdateGroup():
     print("Enter membership change update group")
     await asyncio.sleep(120)
@@ -103,6 +117,7 @@ async def membershipChangeUpdateGroup():
         removeSingleMember(node_id)
     for node_id in ["4", "5", "6"]:
         addSingleMember(node_id)
+
 
 def part1():
     """
@@ -117,10 +132,13 @@ def part1():
     """
     for node_id in ["1", "2", "3"]:
         addSingleMember(node_id)
+
     async def part1a():
         await asyncio.gather(appendEntriesLoop(), membershipChangeUpdateGroup())
+
     asyncio.run(part1a())
-    
+
+
 def part2():
     """
     Part 2:
@@ -139,6 +157,7 @@ def part2():
             print(active_node_ids)
             continue
         appendEntries(active_node_id)
+
 
 def main():
     part1()
