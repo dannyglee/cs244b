2b9fa3615a995fb0535524449ff268af82946c2a
zixi an <tantripplanner@gmail.com>
2022-06-01 23:23:03 -0400

    test data added

--- client_requests/main.py
+++ client_requests/main.py
@@ -11,6 +11,9 @@ import threading
 
 DEBUG = False
 active_node_ids = []
+csv_file_name = ""
+standard_start = datetime.datetime(2022,1,1,0,0,0,0)
+start_time = datetime.datetime.now()
 
 
 def removeSingleMember(node_id):
@@ -48,9 +51,9 @@ def appendEntries(node_id):
     )
 
     elapsed_seconds = response.elapsed.total_seconds()
-    time = datetime.datetime.now(datetime.timezone.utc).astimezone()
+    time = (standard_start + (datetime.datetime.now() - start_time)).astimezone()
 
-    writer.writerow([time.isoformat(), "appendEntries", elapsed_seconds * 1000])
+    writer.writerow([csv_file_name, time.isoformat(), "appendEntries", elapsed_seconds * 1000])
 
     if DEBUG:
         print(time.isoformat(), "appendEntries", elapsed_seconds * 1000)
@@ -65,9 +68,9 @@ def addSingleMember(node_id):
     )
 
     elapsed_seconds = response.elapsed.total_seconds()
-    time = datetime.datetime.now(datetime.timezone.utc).astimezone()
+    time = (standard_start + (datetime.datetime.now() - start_time)).astimezone()
 
-    writer.writerow([time.isoformat(), "membership", elapsed_seconds * 1000])
+    writer.writerow([csv_file_name, time.isoformat(), "membership", elapsed_seconds * 1000])
 
     if DEBUG:
         print(time.isoformat(), "membership", elapsed_seconds * 1000)
@@ -88,8 +91,8 @@ def updateGroup(node_ids):
         json=body
     )
     elapsed_seconds = response.elapsed.total_seconds()
-    time = datetime.datetime.now(datetime.timezone.utc).astimezone()
-    writer.writerow([time.isoformat(), "membership", elapsed_seconds * 1000])
+    time = (standard_start + (datetime.datetime.now() - start_time)).astimezone()
+    writer.writerow([csv_file_name, time.isoformat(), "membership", elapsed_seconds * 1000])
     if DEBUG:
         print(time.isoformat(), "membership", elapsed_seconds * 1000)
 
@@ -111,7 +114,9 @@ def part1():
     async def appendEntriesLoop():
         # TODO: Increase QPS to 100.
         async with aiohttp.ClientSession() as session:
-            while True:
+            i=0
+            while i < 7000:
+                i+=1
                 node_id = selectActiveNodeId()
                 if node_id == -1:
                     continue
@@ -122,11 +127,9 @@ def part1():
                     f"http://{public_node_ip}:{constants.NODE_PORT}/add?command={command}"
                 ) as resp:
                     end = time.time()
-                    current_time = datetime.datetime.now(
-                        datetime.timezone.utc
-                    ).astimezone()
+                    current_time = (standard_start + (datetime.datetime.now() - start_time)).astimezone()
                     writer.writerow(
-                        [current_time.isoformat(), "appendEntries", end - start]
+                        [csv_file_name, current_time.isoformat(), "appendEntries", end - start]
                     )
                     if DEBUG:
                         print(current_time.isoformat(), "appendEntries", end - start)
@@ -138,24 +141,24 @@ def part1():
         loop.close()
 
     def membershipChangeUpdateGroup():
-        print("stuff")
         time.sleep(120)
         print("started membership changes")
-        updateGroup(["4","5","6"])
-        # for node_id in ["4", "5", "6"]:
-        #     addSingleMember(node_id)
-        #     time.sleep(1)
-        # for node_id in ["1", "2", "3"]:
-        #     removeSingleMember(node_id)
-        #     time.sleep(1)
+        # updateGroup(["4","5","6"])
+        for node_id in ["4", "5", "6"]:
+            addSingleMember(node_id)
+        for node_id in ["1", "2", "3"]:
+            removeSingleMember(node_id)
         return
 
     for node_id in ["1", "2", "3"]:
         addSingleMember(node_id)
 
     threading.Thread(target=between_callback).start()
+    # threading.Thread(target=between_callback).start()
+    # threading.Thread(target=between_callback).start()
+    # threading.Thread(target=between_callback).start()
+    # threading.Thread(target=between_callback).start()
     threading.Thread(membershipChangeUpdateGroup())
-
 
 def part2():
     """
@@ -163,31 +166,31 @@ def part2():
     initialize all 7 members -> manually kill registry through AWS shell
     test appendEntries
     """
-    for node_id in range(1, 8):
+    for node_id in range(1, 6):
         addSingleMember(str(node_id))
-        time.sleep(1)
+        time.sleep(0.1)
 
     # Manually kill registry through AWS shell.
-    time.sleep(10)
+    time.sleep(1)
     # TODO: Increase QPS to 100.
     async def appendEntriesLoop():
         async with aiohttp.ClientSession() as session:
-            while True:
+            i=0
+            while i < 8000:
+                i += 1
                 node_id = selectActiveNodeId()
                 if node_id == -1:
                     continue
-                public_node_ip = constants.PUBLIC_NODE_IPS[node_id]
+                public_node_ip = constants.PUBLIC_NODE_IPS["1"]
                 command = "test"
                 start = time.time()
                 async with session.get(
                     f"http://{public_node_ip}:{constants.NODE_PORT}/add?command={command}"
                 ) as resp:
                     end = time.time()
-                    current_time = datetime.datetime.now(
-                        datetime.timezone.utc
-                    ).astimezone()
+                    current_time = (standard_start + (datetime.datetime.now() - start_time)).astimezone()
                     writer.writerow(
-                        [current_time.isoformat(), "appendEntries", end - start]
+                        [csv_file_name, current_time.isoformat(), "appendEntries", end - start]
                     )
                     if DEBUG:
                         print(current_time.isoformat(), "appendEntries", end - start)
@@ -197,8 +200,8 @@ def part2():
 
 
 def main():
-    part1()
-    # part2()
+    # part1()
+    part2()
 
 
 if __name__ == "__main__":
